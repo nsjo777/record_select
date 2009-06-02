500cf380744de86f81754da455239785cfa1fc9a
Jorgen H. Fjeld <jorgen@veridit.no>
2009-06-02 21:17:31 +0200

    Changed to case insensitive search.

--- lib/record_select/conditions.rb
+++ lib/record_select/conditions.rb
@@ -32,7 +32,7 @@ module RecordSelect
       if params[:search] and !params[:search].strip.empty?
         tokens = params[:search].strip.split(' ')
 
-        where_clauses = record_select_config.search_on.collect { |sql| "#{sql} LIKE ?" }
+        where_clauses = record_select_config.search_on.collect { |sql| "#{sql} ILIKE ?" }
         phrase = "(#{where_clauses.join(' OR ')})"
 
         sql = ([phrase] * tokens.length).join(' AND ')
